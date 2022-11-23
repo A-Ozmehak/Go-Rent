a0b4e551d964ed9ae4cc06a3811330d510c16bbb
felixloot <felix.bakkum2002@gmail.com>
2022-11-23 17:09:04 +0100

    Fixed ID as key.

--- mockData.ts
+++ mockData.ts
@@ -1,30 +1,35 @@
 export const placeholderListing = [
     {
         "title": "Mysig lokal för fest",
+        "id": 1,
         "location": "Kärra - Göteborg",
         "price": "1200kr/dygn",
         "imageSrc": "https://images.ctfassets.net/pg6xj64qk0kh/2r4QaBLvhQFH1mPGljSdR9/39b737d93854060282f6b4a9b9893202/camden-paces-apartments-buckhead-ga-terraces-living-room-with-den_1.jpg"
     },
     {
         "title": "Mysig lokal för fest",
+        "id": 2,
         "location": "Kärra - Göteborg",
         "price": "1200kr/dygn",
         "imageSrc": "https://images.ctfassets.net/pg6xj64qk0kh/2r4QaBLvhQFH1mPGljSdR9/39b737d93854060282f6b4a9b9893202/camden-paces-apartments-buckhead-ga-terraces-living-room-with-den_1.jpg"
     },
     {
         "title": "Mysig lokal för fest",
+        "id": 3,
         "location": "Kärra - Göteborg",
         "price": "1200kr/dygn",
         "imageSrc": "https://images.ctfassets.net/pg6xj64qk0kh/2r4QaBLvhQFH1mPGljSdR9/39b737d93854060282f6b4a9b9893202/camden-paces-apartments-buckhead-ga-terraces-living-room-with-den_1.jpg"
     },
     {
         "title": "Mysig lokal för fest",
+        "id": 4
         "location": "Kärra - Göteborg",
         "price": "1200kr/dygn",
         "imageSrc": "https://images.ctfassets.net/pg6xj64qk0kh/2r4QaBLvhQFH1mPGljSdR9/39b737d93854060282f6b4a9b9893202/camden-paces-apartments-buckhead-ga-terraces-living-room-with-den_1.jpg"
     },
     {
         "title": "Mysig lokal för fest",
+        "id": 5,
         "location": "Kärra - Göteborg",
         "price": "1200kr/dygn",
         "imageSrc": "https://images.ctfassets.net/pg6xj64qk0kh/2r4QaBLvhQFH1mPGljSdR9/39b737d93854060282f6b4a9b9893202/camden-paces-apartments-buckhead-ga-terraces-living-room-with-den_1.jpg"
