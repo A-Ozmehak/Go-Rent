760f044d1661832fa62ae68f9196ceec8c5111c1
Anna Özmehak <90522443+A-Ozmehak@users.noreply.github.com>
2022-11-23 16:38:37 +0100

    added login form with validation, a textinput component and in the primarybutton component a chakra button

--- components/buttons/PrimaryButton.tsx
+++ components/buttons/PrimaryButton.tsx
@@ -2,9 +2,13 @@ import { Button } from "@chakra-ui/react";
 import React from "react";
 
 // the blue buttons, register, login, readmore, submit, send and such, send title as props
+type Props = {
+  title: string;
+  type: "submit"
+}
 
-const PrimaryButton = () => {
-  return <Button variant="primary"></Button>;
+const PrimaryButton = ({title, type}: Props) => {
+  return <Button variant="primary" type={type}>{title}</Button>;
 };
 
 export default PrimaryButton;
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,79 @@
 import React from "react";
+import TextInput from "../components/inputs/TextInput";
+import {Formik} from "formik"
+import PrimaryButton from '../components/buttons/PrimaryButton';
+import ShadowContainer from "../components/layout/ShadowContainer";
+import {FormControl, FormLabel, Input, FormErrorMessage} from "@chakra-ui/react";
+
 
 const LoginPage = () => {
-  return null;
-};
+    const passwordLink = {
+        textDecoration: "underline",
+    }
 
-export default LoginPage;
+    return (
+        <ShadowContainer>
+            <h1>Logga in</h1>
+            <h2>För att hyra eller hyra ut hos oss, logga in eller registrera dig nedan.</h2>
+            <div>
+                <Formik
+                    initialValues={{
+                        email: "",
+                        password: "",
+                    }}
+                    onSubmit={(values) => {
+
+                    }}
+                >
+                    {({handleSubmit, errors, touched}) => (
+                        <form onSubmit={handleSubmit}>
+                            <FormControl isInvalid={!!errors.email && touched.email}>
+                                <FormLabel htmlFor="email">Email</FormLabel>
+                                <TextInput
+                                    as={Input}
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    variant="filled"
+                                    validate={(value: string) => {
+                                        let error;
+                                        if (value.length < 5) {
+                                            error = "Skriv in en email"
+                                        }
+                                        return error
+                                    }}
+                                    />
+                                <FormErrorMessage>{errors.email}</FormErrorMessage>
+                            </FormControl>
+                            <FormControl isInvalid={!!errors.password && touched.password}>
+                                <FormLabel htmlFor="password">Lösenord</FormLabel>
+                                <TextInput
+                                    as={Input}
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    variant="filled"
+                                    validate={(value: string) => {
+                                        let error;
+                                        if (value.length < 5) {
+                                            error = "Lösenordet måste vara längre än 6 karaktärer"
+                                        }
+                                        return error
+                                    }}
+
+                                />
+                                <FormErrorMessage>{errors.password}</FormErrorMessage>
+                            </FormControl>
+
+                            <p style={passwordLink}>Glömt ditt lösenord?</p>
+                            <PrimaryButton title="Logga in" type="submit"/>
+                        </form>
+                        )}
+                        </Formik>
+                        </div>
+                        <p>Har du inget konto? Registrera dig.</p>
+                        </ShadowContainer>
+                        )
+                    };
+
+                    export default LoginPage;
