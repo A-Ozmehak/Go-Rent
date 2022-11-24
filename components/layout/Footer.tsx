4970a611914f0b7998040937bc1c399aebbf7af8
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-11-24 20:02:17 +0100

    created footer component

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Box, Text } from "@chakra-ui/react";
 
 const Footer = () => {
-  return
+  return (
+    <footer>
+      <Box paddingTop="1rem" backgroundColor="#F1F1F1" textAlign="center">
+        <Text pb=".3rem">Kontakta oss</Text>
+        <Text pb=".3rem">Logga in / registrera</Text>
+        <Text pb=".3rem">Vanliga frågor</Text>
+        <Text display="flex" justifyContent="center">
+          Copyright 2022 - Göteborgs stadsfilial
+        </Text>
+      </Box>
+    </footer>
+  );
 };
 
 export default Footer;
