275afe56e4d5f2df9b43a5b10d836a562339b011
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-11-24 11:54:59 +0100

    added a comma to fix broken syntax

--- mockData.ts
+++ mockData.ts
@@ -22,7 +22,7 @@ export const placeholderListing = [
     },
     {
         "title": "Mysig lokal för fest",
-        "id": 4
+        "id": 4,
         "location": "Kärra - Göteborg",
         "price": "1200kr/dygn",
         "imageSrc": "https://images.ctfassets.net/pg6xj64qk0kh/2r4QaBLvhQFH1mPGljSdR9/39b737d93854060282f6b4a9b9893202/camden-paces-apartments-buckhead-ga-terraces-living-room-with-den_1.jpg"
