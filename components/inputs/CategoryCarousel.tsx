621c99b4517dcde47cb0264e323a313ff1fd21ff
Prisberg <fille.risberg@live.se>
2022-11-24 23:04:14 +0100

    mobile done

--- components/inputs/CategoryCarousel.tsx
+++ components/inputs/CategoryCarousel.tsx
@@ -2,61 +2,65 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
-import { IconButton, SystemStyleObject } from "@chakra-ui/react";
-import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
+import { IconButton, SystemStyleObject, Text } from "@chakra-ui/react";
+import { DirectionsCar, PhoneIphone, SportsTennis, Checkroom, Hardware, Celebration } from '@mui/icons-material';
 import { Navigation } from "swiper";
 
 
 const categories = [
     {
         title: 'Fordon',
-        icon: DirectionsCarIcon
+        icon: DirectionsCar
     },
     {
         title: 'Teknik',
-        icon: DirectionsCarIcon
+        icon: PhoneIphone
     },
     {
         title: 'Fritid',
-        icon: DirectionsCarIcon
+        icon: SportsTennis
     },
     {
         title: 'Mode',
-        icon: DirectionsCarIcon
+        icon: Checkroom
     },
     {
         title: 'Verktyg',
-        icon: DirectionsCarIcon
+        icon: Hardware
     },
     {
         title: 'Övrigt',
-        icon: DirectionsCarIcon
+        icon: Celebration
     },
 ]
 
 const CategoryCarousel = () => {
 
     const iconButtonStyle: SystemStyleObject = {
-        background: 'blue'
+        background: '#006699',
+        color: 'white',
+        borderRadius: '50%'
     }
 
+    // Use theme instead of manually entering color.
     return (
         <>
+        {/* If mobile then swiper, if not then no swiper */}
             <Swiper
                 navigation={true}
                 modules={[Navigation]}
                 slidesPerView={4}
-                spaceBetween={0}
+                spaceBetween={-20}
                 className="swiperStyle"
             >
                 {categories.map((category) => (
                     <SwiperSlide key={category.title} className="swiperSlide">
-                        <IconButton
-                            sx={iconButtonStyle}
-                            aria-label={"Category select"}
-                            icon={<category.icon />}
-                        />
-                        <span>{category.title}</span>
+                            <IconButton
+                                sx={iconButtonStyle}
+                                aria-label={"Category select"}
+                                icon={<category.icon />}
+                            />
+                            <Text color="#006699">{category.title}</Text>
                     </SwiperSlide>
                 ))}
             </Swiper>
