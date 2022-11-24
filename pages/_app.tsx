22ef742c8aab2f5139dfbd83fce1af68a1cb57da
Anna Özmehak <90522443+A-Ozmehak@users.noreply.github.com>
2022-11-24 19:55:06 +0100

    added form for registration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { ChakraProvider, Heading, Text, Box } from "@chakra-ui/react";
 import { theme } from "../utils/theme";
 import Navbar from "../components/navbar";
+import RegisterPage from "./register";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
