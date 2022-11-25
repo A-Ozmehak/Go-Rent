4b9b5dcdfa24c6cba96cf2c6f970b100570e1656
felixloot <felix.bakkum2002@gmail.com>
2022-11-25 10:19:05 +0100

    Theme updated
    Removed Button & Typo components.
    
    Button Styled with Chakra Theme Variants.
    
    Typo styled with Chakra Theme Global styling on
    H1,
    H2,
    H3,
    P
    Tags
    
    FontSizes are not set.

--- components/typography/PageTitle.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PageTitle = () => {
-  return null;
-};
-
-export default PageTitle;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider, Heading, Text, Box } from "@chakra-ui/react";
-import { theme } from "../utils/theme";
+import { Theme } from "../utils/theme";
 import Navbar from "../components/navbar";
+import "@fontsource/bebas-neue";
+import "@fontsource/inter";
+import "@fontsource/josefin-sans";
+import 'material-icons/iconfont/material-icons.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={Theme}>
       <Navbar />
       <Component {...pageProps} />
     </ChakraProvider>
