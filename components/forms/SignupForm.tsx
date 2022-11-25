0d70fec3841d60654de19173c3988b81378d298c
Prisberg <fille.risberg@live.se>
2022-11-25 10:08:12 +0100

    Merge branch 'main' of https://github.com/A-Ozmehak/Go-Rent into 24-create-category-carousel-component

--- components/forms/SignupForm.tsx
+++ components/forms/SignupForm.tsx
@@ -1,7 +1,105 @@
 import React from "react";
+import {Formik} from "formik";
+import TextInput from "../inputs/TextInput";
+import {FormControl, FormLabel, FormErrorMessage, Input} from "@chakra-ui/react";
+import PrimaryButton from "../buttons/PrimaryButton";
 
 const SignupForm = () => {
-  return null;
+  return (
+
+        <div>
+          <Formik
+              initialValues={{
+                name: "",
+                lastName: "",
+                email: "",
+                password: "",
+              }}
+              onSubmit={(values) => {
+
+              }}
+          >
+            {({handleSubmit, errors, touched}) => (
+                <form onSubmit={handleSubmit}>
+                  <FormControl isInvalid={!!errors.name && touched.name}>
+                    <FormLabel htmlFor="name">Förnamn</FormLabel>
+                    <TextInput
+                        as={Input}
+                        id="name"
+                        name="name"
+                        type="name"
+                        variant="filled"
+                        validate={(value: string) => {
+                          let error;
+                          if (value.length < 2) {
+                            error = "Skriv in ditt namn"
+                          }
+                          return error
+                        }}
+                    />
+                    <FormErrorMessage>{errors.name}</FormErrorMessage>
+                  </FormControl>
+                  <FormControl isInvalid={!!errors.lastName && touched.lastName}>
+                    <FormLabel htmlFor="password">Efternamn</FormLabel>
+                    <TextInput
+                        as={Input}
+                        id="lastName"
+                        name="lastName"
+                        type="lastName"
+                        variant="filled"
+                        validate={(value: string) => {
+                          let error;
+                          if (value.length < 2) {
+                            error = "Skriv in ditt efternamn"
+                          }
+                          return error
+                        }}
+                    />
+                    <FormErrorMessage>{errors.lastName}</FormErrorMessage>
+                  </FormControl>
+                  <FormControl isInvalid={!!errors.email && touched.email}>
+                    <FormLabel htmlFor="password">Email</FormLabel>
+                    <TextInput
+                        as={Input}
+                        id="email"
+                        name="email"
+                        type="email"
+                        variant="filled"
+                        validate={(value: string) => {
+                          let error;
+                          if (value.length < 5) {
+                            error = "Skriv in din email"
+                          }
+                          return error
+                        }}
+                    />
+                    <FormErrorMessage>{errors.email}</FormErrorMessage>
+                  </FormControl>
+                  <FormControl isInvalid={!!errors.password && touched.password}>
+                    <FormLabel htmlFor="password">Lösenord</FormLabel>
+                    <TextInput
+                        as={Input}
+                        id="password"
+                        name="password"
+                        type="password"
+                        variant="filled"
+                        validate={(value: string) => {
+                          let error;
+                          if (value.length < 5) {
+                            error = "Skriv in ditt lösenord"
+                          }
+                          return error
+                        }}
+                    />
+                    <FormErrorMessage>{errors.password}</FormErrorMessage>
+                  </FormControl>
+
+                  <PrimaryButton title="Registrera dig" type="submit"/>
+                </form>
+            )}
+          </Formik>
+        </div>
+  )
 };
 
 
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import ShadowContainer from "../components/layout/ShadowContainer";
+import SignupForm from "../components/forms/SignupForm";
+import Heading from "../components/typography/Heading";
+import PageTitle from "../components/typography/PageTitle";
+import Paragraph from "../components/typography/Paragraph";
 
 const RegisterPage = () => {
-  return null;
+    return (
+        <ShadowContainer>
+            <Heading text="Registrera dig" />
+            <PageTitle text="för att också bli en klimathjälte!"/>
+            <SignupForm/>
+            <Paragraph text="Har du redan ett konto? Logga in!"/>
+        </ShadowContainer>
+    )
 };
 
 export default RegisterPage;
