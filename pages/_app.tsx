f5c5ab133e741323f5f9052e3d428eef9f830237
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-11-25 09:52:57 +0100

    added footer to all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,14 @@ import type { AppProps } from "next/app";
 import { ChakraProvider, Heading, Text, Box } from "@chakra-ui/react";
 import { theme } from "../utils/theme";
 import Navbar from "../components/navbar";
+import Footer from "../components/layout/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Navbar />
       <Component {...pageProps} />
+      <Footer />
     </ChakraProvider>
   );
 }
