5689976208b4bf36cab4357756dfcaa75f5ac77c
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-11-25 09:52:49 +0100

    finished footer component

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import { Box, Text } from "@chakra-ui/react";
+import Link from "next/link";
 
 const Footer = () => {
+  const mediaQueries = {
+    display: { md: "flex" },
+    justifyContent: { md: "space-around" },
+    paddingTop: { md: "3.7rem", sm: "2rem" },
+  };
+
+  const footerStyle = {
+    backgroundColor: "#F1F1F1",
+    textAlign: "center",
+  };
+
+  const copyrightStyle = {
+    marginTop: { sm: ".5rem", md: "2rem" },
+  };
+
   return (
-    <footer>
-      <Box paddingTop="1rem" backgroundColor="#F1F1F1" textAlign="center">
+    <footer style={footerStyle as CSSProperties}>
+      <Box sx={mediaQueries}>
         <Text pb=".3rem">Kontakta oss</Text>
-        <Text pb=".3rem">Logga in / registrera</Text>
-        <Text pb=".3rem">Vanliga frågor</Text>
-        <Text display="flex" justifyContent="center">
-          Copyright 2022 - Göteborgs stadsfilial
+        <Text pb=".3rem">
+          <Link href="/login">
+            <span>Logga in </span>
+          </Link>
+          <span>/ </span>
+          <Link href="/register">
+            <span>Registrera</span>
+          </Link>
         </Text>
+        <Text pb=".3rem">Vanliga frågor</Text>
       </Box>
+      <Text sx={copyrightStyle}>Copyright 2022 - Göteborgs stadsfilial</Text>
     </footer>
   );
 };
