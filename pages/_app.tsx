4dfb81228e1918c3de8c74f856ae0a779ebe85af
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-11-29 13:17:36 +0100

    remove unnused imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.sass";
 import type { AppProps } from "next/app";
-import { ChakraProvider, Heading, Text, Box, Flex } from "@chakra-ui/react";
+import { ChakraProvider, Box, } from "@chakra-ui/react";
 import { Theme } from "../utils/theme";
 import Navbar from "../components/navbar";
 import "@fontsource/bebas-neue";
@@ -8,8 +8,6 @@ import "@fontsource/inter";
 import "@fontsource/josefin-sans";
 import 'material-icons/iconfont/material-icons.css';
 import Footer from "../components/layout/Footer";
-import ProfileCard from "../components/cards/ProfileCard";
-import {profile} from "./api/mockdata";
 import UserProvider from "../utils/userContext";
 
 export default function App({ Component, pageProps }: AppProps) {
