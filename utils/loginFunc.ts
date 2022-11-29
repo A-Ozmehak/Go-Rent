d56ebd6de9e4f8d37cd3e90f0b4716df83fe85d9
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-11-29 16:57:15 +0100

    user routed to profile after login and register

--- utils/loginFunc.ts
+++ utils/loginFunc.ts
@@ -1,40 +1,50 @@
-import { setPersistence, browserSessionPersistence, signInWithEmailAndPassword, getAuth } from "firebase/auth";
+import {
+  setPersistence,
+  browserSessionPersistence,
+  signInWithEmailAndPassword,
+  getAuth,
+} from "firebase/auth";
 import router from "next/router";
 import { app } from "../firebase/firebaseConfig";
 
+const auth = getAuth(app);
 
-const auth = getAuth(app)
+export default function signIn({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  console.log(email, password);
 
-export default function signIn({ email, password }: { email: string; password: string }) {
-    console.log(email, password);
+  setPersistence(auth, browserSessionPersistence)
+    .then(() => {
+      // Existing and future Auth states are now persisted in the current
+      // session only. Closing the window would clear any existing state even
+      // if a user forgets to sign out.
+      // ...
+      // New sign-in will be persisted with session persistence.
+      return signInWithEmailAndPassword(auth, email, password);
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
 
-    setPersistence(auth, browserSessionPersistence)
-        .then(() => {
-            // Existing and future Auth states are now persisted in the current
-            // session only. Closing the window would clear any existing state even
-            // if a user forgets to sign out.
-            // ...
-            // New sign-in will be persisted with session persistence.
-            return signInWithEmailAndPassword(auth, email, password);
-        })
-        .catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-        });
-
-    signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in
-            const user = userCredential.user;
-            console.log(user);
-            // alert("Successfully signed in!");
-            router.push("/");
-            // ...
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            alert(errorCode);
-            //   const errorMessage = error.message;
-        });
-};
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log(user);
+      // alert("Successfully signed in!");
+      router.push("/profile");
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      alert(errorCode);
+      //   const errorMessage = error.message;
+    });
+}
