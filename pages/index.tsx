630df9c9e564b52f9c85671432f4666eb9d42203
Prisberg <fille.risberg@live.se>
2022-11-30 20:30:06 +0100

    call to action style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Head from "next/head";
-import { Text, Box, Flex, Button } from "@chakra-ui/react";
+import { Text, Box, Flex, Button, Heading, SystemStyleFunction, SystemStyleObject, Container } from "@chakra-ui/react";
 import ListingPreviewCard from "../components/cards/ListingPreviewCard";
 import { listingInterface } from "../utils/interface";
 import { getListings } from "./api/listings";
+import { useRouter } from "next/router";
+import { height } from "@mui/system";
 
 
 export async function getStaticProps() {
@@ -14,53 +16,74 @@ export async function getStaticProps() {
   }
 }
 
-export default function Index(props : any) {
-  let listings : listingInterface[] = props.listings
+export default function Index(props: any) {
+  const router = useRouter();
+
+  let listings: listingInterface[] = props.listings
   listings.length = 4
-  return (  
-    <div>
+
+  const CTAboxOuter: SystemStyleObject = {
+    width: "100%",
+    height: "min-content",
+    background: "rgba(0, 0, 0, 0.46)",
+    padding: "0.5rem",
+    justifyContent: "center",
+  }
+
+  const CTAboxInner: SystemStyleObject = {
+    width: { base: "100%", sm: "450px" },
+    flexDirection: { base: "row", sm: "column" },
+    gap: "0.5rem"
+  }
+
+  const CTAbtn: SystemStyleObject = {
+    alignSelf: "flex-end",
+    background: "rgba(255, 255, 255, 0.73)",
+    boxShadow: "4px 4px 4px rgba(0, 0, 0, 0.25)",
+    borderRadius: 0,
+    color: "#234B9A",
+    padding: "0 2rem",
+  }
+
+  return (
+    <Box>
       <Head>
         <title>Go:Rent</title>
         <meta name="description" content="Go:Rent Uthyrning" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="hero">
-        <Box pt="8rem" px="3rem" className="hero-box">
-          <h1>
-            En klimatsatsning från Göteborgs stad, lär dig hur du kan göra
-            skillnad.
-          </h1>
-          <Button variant="Primary">Läs mer</Button>
+      <Box maxW="1200px" m="auto">
+        <Box className="hero">
+          <Box pt="2rem" px="3rem" className="hero-box">
+            <Heading>
+              En klimatsatsning från Göteborgs stad, lär dig hur du kan göra
+              skillnad.
+            </Heading>
+            <Button variant="Primary">Läs mer</Button>
+          </Box>
         </Box>
-      </div>
-      <Text fontSize="2rem" pt="4rem" pl="2rem">
-        Senast upplagt
-      </Text>
-      <ListingPreviewCard listings={listings} />
-      <div className="placeholder">
+        <Text fontSize="2rem" pt="4rem" pl="2rem">
+          Senast upplagt
+        </Text>
+        <ListingPreviewCard listings={listings} />
         <Flex
-          m="0 auto"
+          className="placeholder"
           w="100%"
           h="100%"
-          justifyContent="center"
           alignItems="center"
         >
-          <Flex
-            w="100%"
-            className="placeholderText"
-            justifyContent="center"
-            alignItems="center"
-            h="50%"
-          >
-            <Text color="white" fontSize="2rem">
-              Trångt i garaget?
-            </Text>
-            <Button fontSize="1rem" variant="secondary" ml="2rem">
-              Lägg upp annons
-            </Button>
+          <Flex sx={CTAboxOuter}>
+            <Flex sx={CTAboxInner}>
+              <Heading variant="h2" color="white">
+                Trångt i garaget?
+              </Heading>
+              <Button onClick={() => router.push("/createListing")} sx={CTAbtn}>
+                Lägg upp annons
+              </Button>
+            </Flex>
           </Flex>
         </Flex>
-      </div>
-    </div>
+      </Box>
+    </Box>
   );
 }
