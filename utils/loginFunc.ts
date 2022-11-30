5444acfa62eca0131c8179eba5863e1cdbcd0985
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-11-30 15:31:45 +0100

    removed comments

--- utils/loginFunc.ts
+++ utils/loginFunc.ts
@@ -18,27 +18,18 @@ export default function signIn({
 }) {
   setPersistence(auth, browserSessionPersistence)
     .then(() => {
-      // Existing and future Auth states are now persisted in the current
-      // session only. Closing the window would clear any existing state even
-      // if a user forgets to sign out.
-      // ...
-      // New sign-in will be persisted with session persistence.
       return signInWithEmailAndPassword(auth, email, password);
     })
     .catch((error) => {
-      // Handle Errors here.
       const errorCode = error.code;
       const errorMessage = error.message;
     });
 
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-      // Signed in
       const user = userCredential.user;
       console.log(user);
-      // alert("Successfully signed in!");
       router.push("/profile/" + user.uid);
-      // ...
     })
     .catch((error) => {
       const errorCode = error.code;
