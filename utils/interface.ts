55420054d544498d47d7f6ceffadf53cabe3e9b8
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-11-30 10:42:40 +0100

    rename to userinterface

--- utils/interface.ts
+++ utils/interface.ts
@@ -1,20 +1,26 @@
+import { DocumentData } from "firebase/firestore";
+
 export interface listingInterface {
     title: string,
-    id: string,
-    location: string,
+    id?: string,
+    location?: string,
     price: number,
     media: string,
     description: string,
-    username: string,
-    user?: {
-        image: string,
-        name: string
-    }
+    category: string,
+    // seller id
+    seller: string,
+    // user?: {
+    //     image: string,
+    //     name: string
+    // }
 }
 
-export interface profileInterface {
+export interface userInterface extends DocumentData {
     image?: string;
     username?: string;
+    firstName?: string;
+    lastName?: string;
     location?: string;
     bio?: string;
     id?: string;
@@ -22,7 +28,7 @@ export interface profileInterface {
 
 
 
-export interface CategoryDoc {
+export interface CategoryDoc extends DocumentData {
     id: string,
     icon: string,
     name: string,
