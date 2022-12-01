6979f107642c070c129236b070fbd00dcfa3704d
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-12-01 14:10:20 +0100

    user can only create listing if logged in

--- pages/createListing.tsx
+++ pages/createListing.tsx
@@ -6,29 +6,37 @@ import ShadowContainer from "../components/layout/ShadowContainer";
 import { auth } from "../config/firebase";
 
 const CreateListingPage = () => {
-  const loggedInUser = useAuthState(auth);
+  const [user] = useAuthState(auth);
 
   return (
-    <ShadowContainer>
-      {loggedInUser ? (
-        <ListingForm />
-      ) : (
-        <Flex flexDirection={"column"} alignItems={"center"} gap={3}>
-          <Text textAlign={"center"}>
-            För att föra in en annons hos oss behövs ett konto.
-          </Text>
-          <Text textAlign={"center"}>Logga in eller registrera dig nedan.</Text>
-          <Flex gap={10} marginTop={6}>
-            <Link href="/login">
-              <Button variant="Primary">Logga in</Button>
-            </Link>
-            <Link href="/register">
-              <Button variant="Secondary">Registrera dig</Button>
-            </Link>
-          </Flex>
-        </Flex>
+    <>
+      {user && (
+        <ShadowContainer>
+          <ListingForm />
+        </ShadowContainer>
       )}
-    </ShadowContainer>
+
+      {!user && (
+        <ShadowContainer>
+          <Flex flexDirection={"column"} alignItems={"center"} gap={3}>
+            <Text textAlign={"center"}>
+              För att föra in en annons hos oss behövs ett konto.
+            </Text>
+            <Text textAlign={"center"}>
+              Logga in eller registrera dig nedan.
+            </Text>
+            <Flex gap={10} marginTop={6}>
+              <Link href="/login">
+                <Button variant="Primary">Logga in</Button>
+              </Link>
+              <Link href="/register">
+                <Button variant="Secondary">Registrera dig</Button>
+              </Link>
+            </Flex>
+          </Flex>
+        </ShadowContainer>
+      )}
+    </>
   );
 };
 
