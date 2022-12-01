62daf96bf3150c992a2994324d40339994512441
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-12-01 14:34:34 +0100

    shows you are logged in if u are

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,18 +1,48 @@
 import React from "react";
 import ShadowContainer from "../components/layout/ShadowContainer";
 import SignupForm from "../components/forms/SignupForm";
-import { Heading, Text } from "@chakra-ui/react";
+import { Heading, Text, Flex, Button } from "@chakra-ui/react";
 import Link from "next/link";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../config/firebase";
+import { useRouter } from "next/router";
 
 const RegisterPage = () => {
-    return (
+  const router = useRouter();
+  const [user] = useAuthState(auth);
+
+  return (
+    <>
+      {user && (
         <ShadowContainer>
-            <Heading>Registrera dig</Heading>
-            <Heading>för att också bli en klimathjälte!</Heading>
-            <SignupForm />
-            <Text>Har du redan ett konto? <Link className='inTextLink' href="/login">Logga in!</Link></Text>
+          <Flex flexDirection={"column"} alignItems={"center"} gap={3}>
+            <Text textAlign={"center"}>Du är redan inloggad!</Text>
+            <Flex marginTop={6}>
+              <Button
+                onClick={() => router.push("/profile/" + user.uid)}
+                variant="Primary"
+              >
+                Min profil
+              </Button>
+            </Flex>
+          </Flex>
         </ShadowContainer>
-    )
+      )}
+      {!user && (
+        <ShadowContainer>
+          <Heading>Registrera dig</Heading>
+          <Heading>för att också bli en klimathjälte!</Heading>
+          <SignupForm />
+          <Text>
+            Har du redan ett konto?{" "}
+            <Link className="inTextLink" href="/login">
+              Logga in!
+            </Link>
+          </Text>
+        </ShadowContainer>
+      )}
+    </>
+  );
 };
 
 export default RegisterPage;
