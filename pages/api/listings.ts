2236313be37031b9d71b1ac6eca4af51757e743d
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-12-02 13:28:51 +0100

    await on getUser

--- pages/api/listings.ts
+++ pages/api/listings.ts
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { collection, getDocs } from 'firebase/firestore';
-import type { NextApiRequest, NextApiResponse } from 'next'
 import { db } from '../../config/firebase';
 import { listingInterface } from '../../utils/interface';
 import { getUser } from './users';
@@ -9,30 +8,31 @@ const listingsCollection = collection(db, "listing");
 
 export const getListings = async () => {
     const documents = await getDocs(listingsCollection)
-    let listings : any = []
-    documents.forEach(doc => {
+    let listings: any = []
+
+    for (const doc of documents.docs) {
         let listing = doc.data()
-        listing = {...listing, "id": doc.id}
-        listing.seller = JSON.stringify(getUser(listing.seller))
+        listing = { ...listing, "id": doc.id }
+        listing.seller = await getUser(listing.seller)
         listings.push(listing)
-    });
+    }
     return listings
 };
 
 
-export const getListingsByUser = async (id : string) => {
+export const getListingsByUser = async (id: string) => {
     let listings = await getListings()
-    let userListings : any = []
-    for(let listing of listings) {
-        if(listing.seller === id) {
+    let userListings: any = []
+    for (let listing of listings) {
+        if (listing.seller === id) {
             userListings.push(listing)
         }
     }
     return userListings
 }
 
-export const getListing = async (id : string) => {
-    let listings : listingInterface[] = await getListings()
+export const getListing = async (id: string) => {
+    let listings: listingInterface[] = await getListings()
     let listing = listings.find(item => item.id === id)
     return listing
 }
