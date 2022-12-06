d305889e36b8f2b07080fb93563eb48522a4f5c4
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-12-06 17:19:42 +0100

    removed imports and the definitive setting of array length

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,9 @@ import {
   Flex,
   Button,
   Heading,
-  SystemStyleFunction,
   SystemStyleObject,
-  Container,
 } from "@chakra-ui/react";
 import ListingPreviewCard from "../components/cards/ListingPreviewCard";
-import { listingInterface } from "../utils/interface";
 import { getListings } from "./api/listings";
 import { useRouter } from "next/router";
 
@@ -22,11 +19,8 @@ export async function getServerSideProps() {
   };
 }
 
-export default function Index(props: any) {
+export default function Index({listings}: any) {
   const router = useRouter();
-
-  let listings: listingInterface[] = props.listings;
-  listings.length = 5;
 
   const readMoreBtn: SystemStyleObject = {
     alignSelf: { base: "end", sm: "center" },
@@ -87,7 +81,8 @@ export default function Index(props: any) {
         <Heading size="md" as="h3" p="4rem 0 0.5rem 0" pl="1rem">
           Senast upplagt
         </Heading>
-        <ListingPreviewCard listings={listings} />
+         <ListingPreviewCard listings={listings} />
+        
         <Flex className="placeholder" w="100%" h="100%" alignItems="center">
           <Flex sx={CTAboxOuter}>
             <Flex sx={CTAboxInner}>
