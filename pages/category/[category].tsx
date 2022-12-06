235dca4d98a28fa24a9475bb5545effc835db6e9
Prisberg <fille.risberg@live.se>
2022-12-06 19:59:53 +0100

    category page shows results

--- pages/category/[category].tsx
+++ pages/category/[category].tsx
@@ -1,19 +1,29 @@
-import { Container } from "@chakra-ui/react";
+import { Box, Container, Heading } from "@chakra-ui/react";
+import ListingPreviewCard from "../../components/cards/ListingPreviewCard";
+import { CategoryDoc } from "../../utils/interface";
 import { getCategory } from "../api/categories";
+import { getListings } from "../api/listings";
+import listings from "../listings";
 
-const CategoryPage = ({ category }: any) => {
+const CategoryPage = ({ filteredListings, category }: any) => {
+  
   return (
-    <Container>
-      <h1>{category.name}</h1>
-    </Container>
+    <Box pt="1rem" maxW="1200px" m="auto">
+      <Heading size="md" as="h3" p="0rem 0 0.5rem 0" pl="1rem">
+        {category.name}
+      </Heading>
+      <ListingPreviewCard listings={filteredListings} />
+    </Box>
   );
 };
 export default CategoryPage;
 
 export async function getServerSideProps({ params }: any) {
-  const category = await getCategory(params.category);
-
+  const category = await getCategory(params.category) as CategoryDoc;
+  const listings = await getListings()
+  
+  const filteredListings = listings.filter((listing: any) => listing.category === (category ? category.name : ""))
   return {
-    props: { category },
+    props: { filteredListings, category },
   };
 }
