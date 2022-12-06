5c43e2886c5642a90440e40be76831b6f146f2ed
felixloot <felix.bakkum2002@gmail.com>
2022-12-06 11:40:27 +0100

    User gets first letter in name if no image found
    If user have no profile picture they will have their first letter in their firstname as "profile picture"

--- components/cards/MinimalProfileCard..tsx
+++ components/cards/MinimalProfileCard..tsx
@@ -1,4 +1,4 @@
-import { Card, Image } from "@chakra-ui/react";
+import { Card, Image, Text } from "@chakra-ui/react";
 import { userInterface } from "../../utils/interface";
 import { profileImageStyle, card } from "./ProfileCard";
 
@@ -14,7 +14,10 @@ const MinimalProfileCard = ({ profile }: MinimalProfileCardProps) => {
       overflow="hidden"
       variant="outline"
     >
-      <Image sx={profileImageStyle} src={profile.image} alt="profile picture" />
+    {profile.image?.length
+    ? <Image sx={profileImageStyle} src={profile.image} alt="profile picture" />
+    : <Text sx={{p: "1rem", bg: "lightGray"}}>{profile.firstName?.charAt(0)}</Text>
+    }
       <h3>{profile.username}</h3>
     </Card>
   );
