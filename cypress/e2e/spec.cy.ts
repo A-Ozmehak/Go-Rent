b4e8aa80d9c98c7d930764968d62b133dd5401ae
Prisberg <fille.risberg@live.se>
2022-12-13 12:52:39 +0100

    "Log in" and "Create listing" test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,15 +1,35 @@
-describe('Go Rent website', () => {
-  // Open the website before running the tests
+export { }
+
+describe('"Log in" and "Create listing" test', () => {
   beforeEach(() => {
+    // Doing all this beforeEach cause cypress clears cache and leaves site inbetween tests and I can't find where to disable this behaviour...
     cy.on('uncaught:exception', handleUncaughtException);
-    cy.visit('https://go-rent.vercel.app/');
+    // Visit the website
+    cy.visit('https://go-rent.vercel.app')
+    // Go to login
+    cy.get('.chakra-menu__menu-button').first().click();
+    cy.get('body').contains('Logga in').click();
+
+    // Login
+    cy.get('#email').type('lol@live.se')
+    cy.get('#password').type('testtest')
+    cy.get('button[type="submit"]').contains('Logga in').click()
   });
 
-  it('should display the correct title', () => {
-    // Use the `getByText` command to assert that the correct title is displayed
-    cy.getByText('GO:RENT').should('be.visible');
-  });
-});
+  // Login
+  it('Logs in', () => {
+  })
+
+  it('Create a listing', () => {
+    // Go to listing and fill in form
+    // Waiting for redirect to profile page, wait should not be necessary
+    cy.wait(3000).get('button[type="button"]').contains('Lägg upp annons').first().click()
+    cy.get('#title').type('Cypress test-listing')
+    cy.get('#price').type(`${Math.floor(Math.random() * 1000)}`)
+    cy.get('#description').type(`Cypress test-listing, random nr:${Math.floor(Math.random() * 150)}`)
+    cy.get('button[type="submit').contains('Skapa annons').click();
+  })
+})
 
 function handleUncaughtException(error: Error) {
   console.error(error);
